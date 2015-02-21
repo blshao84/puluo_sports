0f160a0659934760fea2669df5115eb388ab2489
blshao84 <b.l.shao84@gmail.com>
2015-02-21 16:32:28 +0800

    add file based db and add test case to cover it

--- puluo_app_backend/core/src/test/java/com/puluo/dao/impl/DaoTestApi.java
+++ puluo_app_backend/core/src/test/java/com/puluo/dao/impl/DaoTestApi.java
@@ -3,6 +3,9 @@ package com.puluo.dao.impl;
 import com.puluo.dao.PgDummyDao;
 
 public class DaoTestApi {
-	public static PgDummyDao pgDummy = 
+	public static PgDummyDao pgInMemDummy = 
 			BeanTestFactory.getBean(PgDummyDao.class,"pgDummyDao");
+	
+	public static PgDummyDao pgFileDummy = 
+			BeanTestFactory.getBean(PgDummyDao.class,"pgDummyDao2");
 }
--- puluo_app_backend/core/src/test/java/com/puluo/test/entity/DummyEntitySaveLoadTest.java
+++ puluo_app_backend/core/src/test/java/com/puluo/test/entity/DummyEntitySaveLoadTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.puluo.dao.PgDummyDao;
 import com.puluo.dao.impl.DaoTestApi;
 import com.puluo.entity.PgDummy;
 import com.puluo.entity.impl.PgDummyImpl;
@@ -12,21 +13,43 @@ import com.puluo.entity.impl.PgDummyImpl;
 public class DummyEntitySaveLoadTest {
 	@BeforeClass
 	public static void setUpDB() {
-		PgDummyImpl dummy1 = new PgDummyImpl(1L, "a");
-		PgDummyImpl dummy2 = new PgDummyImpl(2L, "b");
-		DaoTestApi.pgDummy.createTable();
-		DaoTestApi.pgDummy.save(dummy1);
-		DaoTestApi.pgDummy.save(dummy2);
+		populateDB(DaoTestApi.pgInMemDummy);
 	}
 
 	@Test
-	public void testEntityLoad() {
-		PgDummy dummy1 = DaoTestApi.pgDummy.getById(1L);
-		PgDummy dummy2 = DaoTestApi.pgDummy.getById(2L);
+	public void testInMemEntityLoad() {
+		PgDummy dummy1 = DaoTestApi.pgInMemDummy.getById(1L);
+		PgDummy dummy2 = DaoTestApi.pgInMemDummy.getById(2L);
 		assertEquals("dummy1's id should be a", dummy1.id().longValue(), 1L);
 		assertEquals("dummy2's id should be a", dummy2.id().longValue(), 2L);
 		assertEquals("dummy1's name should be a", dummy1.name(), "a");
 		assertEquals("dummy2's name should be b", dummy2.name(), "b");
+	}
+
+	@Test
+	public void testFileEntityLoad() {
+		PgDummy dummy1 = DaoTestApi.pgFileDummy.getById(1L);
+		PgDummy dummy2 = DaoTestApi.pgFileDummy.getById(2L);
+		assertEquals("dummy1's id should be a", dummy1.id().longValue(), 1L);
+		assertEquals("dummy2's id should be a", dummy2.id().longValue(), 2L);
+		assertEquals("dummy1's name should be a", dummy1.name(), "a");
+		assertEquals("dummy2's name should be b", dummy2.name(), "b");
+	}
+
+	public static void main(String[] args) {
+		populateFileDB();
+	}
+
+	private static void populateFileDB() {
+		populateDB(DaoTestApi.pgFileDummy);
+	}
+
+	private static void populateDB(PgDummyDao dao) {
+		PgDummyImpl dummy1 = new PgDummyImpl(1L, "a");
+		PgDummyImpl dummy2 = new PgDummyImpl(2L, "b");
+		dao.createTable();
+		dao.save(dummy1);
+		dao.save(dummy2);
 	}
 
 }
