c27cf6e9d8ad82096d5cbe6e9676674d5bf7255c
blshao84 <b.l.shao84@gmail.com>
2015-02-27 09:13:21 +0800

    订单模块设计文档

--- puluo_app_backend/core/src/main/java/com/puluo/entity/payment/PuluoOrderStatus.java
+++ puluo_app_backend/core/src/main/java/com/puluo/entity/payment/PuluoOrderStatus.java
@@ -1,3 +1,19 @@
 package com.puluo.entity.payment;
 
+/**
+ * Undefined: Any unexpected state, eq to 'null' state
+ * 
+ * New: After a new order is placed
+ * 
+ * Paying: Go to 3rd party payment service
+ *
+ * Paid: Receive payment confirmation from 3rd party payment service
+ * 
+ * Complete: Event is over (marked by user)
+ * 
+ * Cancel: After user cancel the event and payment
+ * 
+ * @author blshao
+ *
+ */
 public enum PuluoOrderStatus { Undefined, New, Paying, Paid, Complete, Cancel }
