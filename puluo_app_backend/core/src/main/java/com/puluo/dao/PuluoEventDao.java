ca51d0d4f9f66dc8d3360ffc9c558d1d2aab3cf9
blshao84 <b.l.shao84@gmail.com>
2015-02-28 13:51:35 +0800

    remove wrong code …

--- puluo_app_backend/core/src/main/java/com/puluo/dao/PuluoEventDao.java
+++ puluo_app_backend/core/src/main/java/com/puluo/dao/PuluoEventDao.java
@@ -1,20 +1,5 @@
 package com.puluo.dao;
 
-import com.puluo.dao.impl.BeanFactory;
 
 public interface PuluoEventDao {
-	public static PuluoUserDao userDao = 
-			BeanFactory.getBean(PuluoUserDao.class,"userDao");
-	public static PuluoEventDao eventDao = 
-			BeanFactory.getBean(PuluoEventDao.class,"eventDao");
-	public static PuluoEventLocationDao eventLocDao = 
-			BeanFactory.getBean(PuluoEventLocationDao.class,"eventLocDao");
-	public static PuluoPaymentDao paymentDao = 
-			BeanFactory.getBean(PuluoPaymentDao.class,"paymentDao");
-	public static PuluoPrivateMessageDao privateMsgDao = 
-			BeanFactory.getBean(PuluoPrivateMessageDao.class,"privateMsgDao");
-	public static PuluoPostDao postDao = 
-			BeanFactory.getBean(PuluoPostDao.class,"postDao");
-	public static PuluoReservationDao reservationDao = 
-			BeanFactory.getBean(PuluoReservationDao.class,"reservationDao");
 }
