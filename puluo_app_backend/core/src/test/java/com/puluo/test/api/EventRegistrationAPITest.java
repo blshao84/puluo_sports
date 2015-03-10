f6515ff673583238af7397fc71a9d41677080298
blshao84 <b.l.shao84@gmail.com>
2015-03-10 09:23:09 +0800

    Add complete test cases to cover EventRegistrationAPI

--- puluo_app_backend/core/src/test/java/com/puluo/test/api/EventRegistrationAPITest.java
+++ puluo_app_backend/core/src/test/java/com/puluo/test/api/EventRegistrationAPITest.java
@@ -42,6 +42,8 @@ public class EventRegistrationAPITest {
 	private final PuluoOrderEventDao mockOrderEventDao = Mockito
 			.mock(PuluoOrderEventDaoImpl.class);
 
+	private final String eventUUID = "1";
+
 	private class MockDSI extends PuluoDSI {
 
 		@Override
@@ -154,14 +156,114 @@ public class EventRegistrationAPITest {
 
 	@Test
 	public void testCreateOrderWithEvent() {
-		String eventUUID = "1";
 		PuluoEvent event = Mockito.mock(PuluoEventImpl.class);
 		PuluoPaymentOrder order = Mockito.mock(PuluoPaymentOrderImpl.class);
-		Mockito.when(order.amount()).thenReturn(50.0);
-		Mockito.when(order.orderNumericID()).thenReturn(9999L);
-		Mockito.when(order.status()).thenReturn(PuluoOrderStatus.Undefined);
+		setupDao(event, order, false);
+		setupEvent(event);
+		setupOrder(order, PuluoOrderStatus.Undefined);
+		EventRegistrationAPI api = new EventRegistrationAPI("1", "1", mockDsi);
+		api.execute();
+		assertPaymentlink(api.result());
+	}
+
+	@Test
+	public void testDifferentUserInOrderAndAPI() {
+		PuluoPaymentOrder order = Mockito.mock(PuluoPaymentOrderImpl.class);
+		Mockito.when(order.userId()).thenReturn("2");
+		Mockito.when(mockPaymentDao.getOrderByEvent(Matchers.anyString()))
+				.thenReturn(order);
+		EventRegistrationAPI api = new EventRegistrationAPI("1", "1", mockDsi);
+		api.execute();
+		String expectedErrorMessage = "{\"id\":\"系统支付错误\",\"message\":\"订单中的用户id与该用户不匹配\",\"url\":\"\"}";
+		Assert.assertEquals("unexpected error message", expectedErrorMessage,
+				api.result());
+	}
+
+	@Test
+	public void testCanceledOrder() {
+		PuluoPaymentOrder order = Mockito.mock(PuluoPaymentOrderImpl.class);
+		setupOrder(order, PuluoOrderStatus.Cancel);
+		Mockito.when(mockPaymentDao.getOrderByEvent(eventUUID)).thenReturn(
+				order);
+		String actualErrorMessage = run();
+		String expectedErrorMessage = "{\"id\":\"系统支付错误\",\"message\":\"订单已取消\",\"url\":\"\"}";
+		Assert.assertEquals("unexpected error message", expectedErrorMessage,
+				actualErrorMessage);
+	}
+
+	@Test
+	public void testPaidOrder() {
+		PuluoPaymentOrder order = Mockito.mock(PuluoPaymentOrderImpl.class);
+		setupOrder(order, PuluoOrderStatus.Paid);
+		Mockito.when(mockPaymentDao.getOrderByEvent(eventUUID)).thenReturn(
+				order);
+		String actualResult = run();
+		assertPaid(actualResult);
+	}
+
+	@Test
+	public void testCompleteOrder() {
+		PuluoPaymentOrder order = Mockito.mock(PuluoPaymentOrderImpl.class);
+		setupOrder(order, PuluoOrderStatus.Complete);
+		Mockito.when(mockPaymentDao.getOrderByEvent(eventUUID)).thenReturn(
+				order);
+		String actualResult = run();
+		assertPaid(actualResult);
+	}
+
+	@Test
+	public void testUndefinedOrder() {
+		String actualResult = runUpdateWithStatus(PuluoOrderStatus.Undefined);
+		assertPaymentlink(actualResult);
+	}
+
+	@Test
+	public void testNewOrder() {
+		String actualResult = runUpdateWithStatus(PuluoOrderStatus.New);
+		assertPaymentlink(actualResult);
+	}
+
+	@Test
+	public void testPayingOrder() {
+		String actualResult = runUpdateWithStatus(PuluoOrderStatus.Paying);
+		assertPaymentlink(actualResult);
+	}
+
+	private void assertPaid(String actualResult) {
+		String expectedResult = "{\"link\":\"\",\"paid\":true}";
+		Assert.assertEquals(
+				"unexpected result: should return nothing because order is processed",
+				expectedResult, actualResult);
+	}
+
+	private void assertPaymentlink(String actualResult) {
+		String expectedPaymentLink = "https://mapi.alipay.com/gateway.do?_input_charset=utf-8&body=19999&notify_url=/payment/alipay/notify&out_trade_no=19999&partner=2088211443186240&payment_type=1&return_url=/payment/alipay/return&seller_email=dsjtdqxmt@163.com&service=create_direct_pay_by_user&sign=8ca5266c0ae4e6c16189c5f98694b984&sign_type=MD5&subject=19999&total_fee=50.0";
+		Assert.assertTrue("generated link are not expected",
+				actualResult.contains(expectedPaymentLink));
+	}
+
+	private String runUpdateWithStatus(PuluoOrderStatus status) {
+		PuluoEvent event = Mockito.mock(PuluoEventImpl.class);
+		PuluoPaymentOrder order = Mockito.mock(PuluoPaymentOrderImpl.class);
+		setupDao(event, order, true);
+		setupEvent(event);
+		setupOrder(order, status);
+		return run();
+	}
+
+	private String run() {
+		EventRegistrationAPI api = new EventRegistrationAPI("1", "1", mockDsi);
+		api.execute();
+		return api.result();
+	}
+
+	private void setupDao(PuluoEvent event, PuluoPaymentOrder order,
+			boolean getOrderByEvent) {
 		Mockito.when(mockEventDao.getEventByUUID(eventUUID)).thenReturn(event);
-		Mockito.when(event.price()).thenReturn(50.0);
+		if (getOrderByEvent) {
+			Mockito.when(mockPaymentDao.getOrderByEvent(eventUUID)).thenReturn(
+					order);
+		}
 		Mockito.when(mockPaymentDao.getOrderByUUID(Matchers.anyString()))
 				.thenReturn(order);
 		Mockito.when(
@@ -175,10 +277,17 @@ public class EventRegistrationAPITest {
 				mockPaymentDao.updateOrderStatus(
 						Matchers.any(PuluoPaymentOrderImpl.class),
 						Matchers.any(PuluoOrderStatus.class))).thenReturn(true);
-		EventRegistrationAPI api = new EventRegistrationAPI("1", "1", mockDsi);
-		api.execute();
-		String expectedPaymentLink = "https://mapi.alipay.com/gateway.do?_input_charset=utf-8&body=19999&notify_url=/payment/alipay/notify&out_trade_no=19999&partner=2088211443186240&payment_type=1&return_url=/payment/alipay/return&seller_email=dsjtdqxmt@163.com&service=create_direct_pay_by_user&sign=8ca5266c0ae4e6c16189c5f98694b984&sign_type=MD5&subject=19999&total_fee=50.0";
-		Assert.assertTrue("generated link are not expected", api.result()
-				.contains(expectedPaymentLink));
+	}
+
+	private void setupOrder(PuluoPaymentOrder order, PuluoOrderStatus status) {
+		Mockito.when(order.userId()).thenReturn("1");
+		Mockito.when(order.status()).thenReturn(status);
+		Mockito.when(order.amount()).thenReturn(50.0);
+		Mockito.when(order.orderNumericID()).thenReturn(9999L);
+
+	}
+
+	private void setupEvent(PuluoEvent event) {
+		Mockito.when(event.price()).thenReturn(50.0);
 	}
 }
