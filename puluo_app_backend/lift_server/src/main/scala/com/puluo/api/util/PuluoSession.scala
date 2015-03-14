4b6e8e9cb5b62e8f7a6d4131a7a87bb560b9820a
blshao84 <b.l.shao84@gmail.com>
2015-03-14 15:36:35 +0800

    missed check in …

--- puluo_app_backend/lift_server/src/main/scala/com/puluo/api/util/PuluoSession.scala
+++ puluo_app_backend/lift_server/src/main/scala/com/puluo/api/util/PuluoSession.scala
@@ -1,6 +1,7 @@
 package com.puluo.api.util
 
 import net.liftweb.http.SessionVar
+import com.puluo.dao.impl.DaoApi
 
 /**
  * Use this page's mechanism to do session-based authentication 
@@ -8,7 +9,8 @@ import net.liftweb.http.SessionVar
  */
 object PuluoSession extends SessionVar(SessionInfo(None)){
   def login = is.session.isDefined
-  def userUUID = session.userUUID()
+  def userMobile = session.userMobile()
+  def userUUID = DaoApi.getInstance().userDao().getByMobile(userMobile).userUUID()
   def session = is.session.get
 }
 
