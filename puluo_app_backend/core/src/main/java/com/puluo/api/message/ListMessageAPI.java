1350e7f3b9b867b55ca8ebcf409dd161c54d5f06
blshao84 <b.l.shao84@gmail.com>
2015-03-26 08:32:50 +0800

    add http req handler for message api

--- puluo_app_backend/core/src/main/java/com/puluo/api/message/ListMessageAPI.java
+++ puluo_app_backend/core/src/main/java/com/puluo/api/message/ListMessageAPI.java
@@ -1,20 +1,24 @@
 package com.puluo.api.message;
 
+import org.joda.time.DateTime;
+
 import com.puluo.api.PuluoAPI;
 import com.puluo.api.result.ListMessageResult;
 import com.puluo.dao.PuluoDSI;
 import com.puluo.dao.impl.DaoApi;
 
 public class ListMessageAPI extends PuluoAPI<PuluoDSI,ListMessageResult> {
-	public String user_uuid;
-	public String since;
+	public String from_user_uuid;
+	public String to_user_uuid;
+	public DateTime since;
 	
-	public ListMessageAPI(String user_uuid, String since){
-		this(user_uuid, since, DaoApi.getInstance());
+	public ListMessageAPI(String from_user_uuid,String to_user_uuid, DateTime since){
+		this(from_user_uuid,to_user_uuid, since, DaoApi.getInstance());
 	}
-	public ListMessageAPI(String user_uuid, String since, PuluoDSI dsi) {
+	public ListMessageAPI(String from_user_uuid,String to_user_uuid, DateTime since, PuluoDSI dsi) {
 		this.dsi = dsi;
-		this.user_uuid = user_uuid;
+		this.from_user_uuid = from_user_uuid;
+		this.to_user_uuid = to_user_uuid;
 		this.since = since;
 	}
 
--- puluo_app_backend/lift_server/src/main/scala/com/puluo/api/message/PuluoMessageAPI.scala
+++ puluo_app_backend/lift_server/src/main/scala/com/puluo/api/message/PuluoMessageAPI.scala
@@ -7,16 +7,58 @@ import net.liftweb.http.js.JsExp.strToJsExp
 import com.puluo.api.util.PuluoResponseFactory
 import com.puluo.api.result.SendMessageResult
 import com.puluo.api.result.ListMessageResult
+import com.puluo.api.util.PuluoAPIUtil
+import net.liftweb.common.Loggable
+import com.puluo.api.util.ErrorResponseResult
+import com.puluo.session.PuluoSessionManager
+import org.joda.time.DateTime
 
-object PuluoMessageAPI extends RestHelper {
+object PuluoMessageAPI extends RestHelper with PuluoAPIUtil with Loggable {
   serve {
     case "users" :: "message" :: "send" :: Nil Put _ => {
-      PuluoResponseFactory.createDummyJSONResponse(SendMessageResult.dummy().toJson())
+      callWithParam(Map(
+        "token" -> ErrorResponseResult(15).copy(message = "token"),
+        "to_uuid" -> ErrorResponseResult(15).copy(message = "to_uuid"),
+        "content" -> ErrorResponseResult(15).copy(message = "content"),
+        "content_type" -> ErrorResponseResult(15).copy(message = "content_type")))(doSendMessage)
     }
     case "users" :: "messages" :: Nil Post _ => {
       PuluoResponseFactory.createDummyJSONResponse(ListMessageResult.dummy().toJson())
     }
-    
-     
+  }
+
+  private def doSendMessage(params: Map[String, String]) = {
+    val toUserUUID = params("to_uuid")
+    val token = params("token")
+    val content = params("content")
+    val contentType = params("content_type")
+    val session = PuluoSessionManager.getSession(token)
+    val fromUserUUID = session.userUUID()
+    //FIXME: should add fromUserUUID
+    val api = new SendMessageAPI( /*fromUserUUID,*/ toUserUUID, content, contentType)
+    safeRun(api)
+    PuluoResponseFactory.createJSONResponse(api, 201)
+  }
+
+  private def doListMessages = {
+    val params = PuluoResponseFactory.createParamMap(Seq("token", "user_uuid", "since"))
+    val token = params("token")
+    val session = PuluoSessionManager.getSession(token)
+    val fromUserUUID = session.userUUID()
+    val toUserUUID = params.getOrElse("user_uuid", "")
+    val since = params.get("since").map {
+      s =>
+        try {
+          Some(new DateTime(s.toLong))
+        } catch {
+          case e: Exception => {
+            logger.warn(s"'since' param has wrong format, expecting unix timestamp, but was $s")
+            None
+          }
+        }
+    }.flatten.getOrElse(null)
+    val api = new ListMessageAPI(fromUserUUID,toUserUUID,since)
+    safeRun(api)
+    PuluoResponseFactory.createJSONResponse(api)
   }
 }
