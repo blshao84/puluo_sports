450fae96b5aabe64867e4d83608aad2f0b4e3bc8
blshao84 <b.l.shao84@gmail.com>
2015-04-07 23:18:44 +0800

    Add functional tests

--- puluo_app_backend/core_test/src/func_test/java/com/puluo/test/functional/APIFunctionalTests.java
+++ puluo_app_backend/core_test/src/func_test/java/com/puluo/test/functional/APIFunctionalTests.java
@@ -1,11 +1,8 @@
 package com.puluo.test.functional;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
 
-@RunWith(Suite.class)
-@SuiteClasses({LoginFunctionalTest.class})
+//@RunWith(Suite.class)
+//@SuiteClasses({LoginFunctionalTest.class})
 public class APIFunctionalTests {
 
 }
