6b54f4e491348b9ed128cbec4791a15480027c26
blshao84 <b.l.shao84@gmail.com>
2015-04-25 07:27:39 +0800

    Add functional test for events/configurations

---
+++ puluo_app_backend/core_test/src/func_test/java/com/puluo/test/functional/EventConfigurationFunctionalTest.java
@@ -0,0 +1,76 @@
+package com.puluo.test.functional;
+
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import com.puluo.dao.impl.DaoApi;
+import com.puluo.dao.impl.PuluoSessionDaoImpl;
+import com.puluo.dao.impl.PuluoUserDaoImpl;
+import com.puluo.entity.PuluoUser;
+import com.puluo.test.functional.util.APIFunctionalTest;
+import com.puluo.test.functional.util.PuluoAuthenticatedFunctionalTestRunner;
+import com.puluo.util.Log;
+import com.puluo.util.LogFactory;
+
+public class EventConfigurationFunctionalTest extends APIFunctionalTest {
+	public static Log log = LogFactory.getLog(EventConfigurationFunctionalTest.class);
+	static String mobile1 = "123456789";
+	static String password1 = "abcdefg";
+
+	@BeforeClass
+	public static void setupDB() {
+		cleanupDB();
+		DaoApi.getInstance().userDao().save(mobile1, password1);
+	}
+
+	@AfterClass
+	public static void cleanupDB() {
+		PuluoUserDaoImpl dao = (PuluoUserDaoImpl) DaoApi.getInstance()
+				.userDao();
+		PuluoSessionDaoImpl sessionDao = (PuluoSessionDaoImpl) DaoApi
+				.getInstance().sessionDao();
+		PuluoUser user = dao.getByMobile(mobile1);
+		if (user != null) {
+			dao.deleteByUserUUID(user.userUUID());
+			sessionDao.obliterateAllSessions(user.mobile());
+		}
+	}
+	
+	@Test
+	public void testConfiguration(){
+		super.runAuthenticatedTest(new PuluoAuthenticatedFunctionalTestRunner() {
+			
+			@Override
+			public void run(String session) throws UnirestException {
+				JsonNode json = callAPI("events/configurations", inputs(session));
+				String expectedCategories = "[\"Others\"]";
+				String expectedLevels = "[\"Level1\",\"Level2\",\"Level3\",\"Level4\"]";
+				String actualCategories = getStringFromJson(json, "categories");
+				String actualLevels = getStringFromJson(json, "levels");
+				Assert.assertEquals("",expectedCategories, actualCategories);
+				Assert.assertEquals("",expectedLevels, actualLevels);
+				
+			}
+			
+			@Override
+			public String password() {
+				return password1;
+			}
+			
+			@Override
+			public String mobile() {
+				return mobile1;
+			}
+			
+			@Override
+			public String inputs(String session) {
+				return String.format("{" + "\"token\":\"%s\"}", session);
+			}
+		});
+	}
+}
