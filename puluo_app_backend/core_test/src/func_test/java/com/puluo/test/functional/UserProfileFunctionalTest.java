b11c9c1eb77317d3fb15c2995cc852a9ac485ac7
Lei Shi <sinorockie@outlook.com>
2015-04-28 20:44:57 +0800

    Functional Test: blshao84/puluo_document#20

--- puluo_app_backend/core_test/src/func_test/java/com/puluo/test/functional/UserProfileFunctionalTest.java
+++ puluo_app_backend/core_test/src/func_test/java/com/puluo/test/functional/UserProfileFunctionalTest.java
@@ -1,5 +1,9 @@
 package com.puluo.test.functional;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.joda.time.DateTime;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -8,10 +12,14 @@ import org.junit.Test;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import com.puluo.dao.impl.DaoApi;
+import com.puluo.dao.impl.PuluoFriendRequestDaoImpl;
+import com.puluo.dao.impl.PuluoPrivateMessageDaoImpl;
 import com.puluo.dao.impl.PuluoSessionDaoImpl;
 import com.puluo.dao.impl.PuluoUserDaoImpl;
 import com.puluo.dao.impl.PuluoUserFriendshipDaoImpl;
 import com.puluo.entity.PuluoUser;
+import com.puluo.entity.impl.PuluoPrivateMessageImpl;
+import com.puluo.enumeration.PuluoMessageType;
 import com.puluo.test.functional.util.APIFunctionalTest;
 import com.puluo.test.functional.util.PuluoAuthenticatedFunctionalTestRunner;
 import com.puluo.util.Log;
@@ -27,14 +35,23 @@ public class UserProfileFunctionalTest extends APIFunctionalTest {
 	static String mobile2 = "234567890";
 	static String uuid2;
 	static String password2 = "bcdefgh";
-	
+
 	static String mobile4 = "4532342367890";
 	static String uuid4;
 	static String password4 = "bcdefgh";
 	
+	static String mobile5 = "6872342367890";
+	static String uuid5;
+	static String password5 = "bcdefgh";
+	
 	static String mobile3 = "345678901";
 	static String uuid3 = "88d8e55d-bc48-4471-bf1a-a0d7066cd8b2";
 	static String sessionId;
+	
+	static String requestId1 = UUID.randomUUID().toString();
+	static String requestId2 = UUID.randomUUID().toString();
+
+	static String messageId = UUID.randomUUID().toString();
 
 	@BeforeClass
 	public static void setupDB() {
@@ -55,7 +72,6 @@ public class UserProfileFunctionalTest extends APIFunctionalTest {
 				null, null, null, null);
 		uuid2 = user2.userUUID();
 		
-		
 		dao.save(mobile4, password4);
 		PuluoUser user4 = dao.getByMobile(mobile4);
 		dao.updateProfile(user4, "user4", "user4", "http://upyun.com/puluo/user4.jpg", "http://upyun.com/puluo/user4.jpg",
@@ -63,7 +79,19 @@ public class UserProfileFunctionalTest extends APIFunctionalTest {
 				null, null, null, null);
 		uuid4 = user4.userUUID();
 		
+		dao.save(mobile5, password5);
+		PuluoUser user5 = dao.getByMobile(mobile5);
+		dao.updateProfile(user5, "user5", "user5", "http://upyun.com/puluo/user5.jpg", "http://upyun.com/puluo/user5.jpg",
+				"i am user5","user5@puluo.com","M", null,
+				null, null, null, null);
+		uuid5 = user5.userUUID();
+		
 		DaoApi.getInstance().friendshipDao().addOneFriend(uuid1, uuid4);
+
+		DaoApi.getInstance().friendRequestDao().saveNewRequest(requestId1, uuid2, uuid1);
+		DaoApi.getInstance().friendRequestDao().saveNewRequest(requestId2, uuid5, uuid1);
+		
+		DaoApi.getInstance().privateMessageDao().saveMessage(new PuluoPrivateMessageImpl(messageId, "Hello, User1!", DateTime.now(), PuluoMessageType.FriendRequest, requestId1, uuid2, uuid1));
 	}
 
 	@AfterClass
@@ -73,6 +101,8 @@ public class UserProfileFunctionalTest extends APIFunctionalTest {
 		PuluoSessionDaoImpl sessionDao = (PuluoSessionDaoImpl) DaoApi
 				.getInstance().sessionDao();
 		PuluoUserFriendshipDaoImpl friendDao = (PuluoUserFriendshipDaoImpl) DaoApi.getInstance().friendshipDao();
+		PuluoFriendRequestDaoImpl requestDao = (PuluoFriendRequestDaoImpl) DaoApi.getInstance().friendRequestDao();
+		PuluoPrivateMessageDaoImpl messageDao = (PuluoPrivateMessageDaoImpl) DaoApi.getInstance().privateMessageDao();
 		
 		PuluoUser user;
 		user = dao.getByMobile(mobile1);
@@ -85,14 +115,24 @@ public class UserProfileFunctionalTest extends APIFunctionalTest {
 			dao.deleteByUserUUID(user.userUUID());
 			sessionDao.obliterateAllSessions(user.mobile());
 		}
-		
+
 		user = dao.getByMobile(mobile4);
+		if (user != null) {
+			dao.deleteByUserUUID(user.userUUID());
+			sessionDao.obliterateAllSessions(user.mobile());
+		}
+		user = dao.getByMobile(mobile5);
 		if (user != null) {
 			dao.deleteByUserUUID(user.userUUID());
 			sessionDao.obliterateAllSessions(user.mobile());
 		}
 		friendDao.deleteByUserUUID(uuid1);
 		friendDao.deleteByUserUUID(uuid4);
+
+		requestDao.deleteByReqUUID(requestId1);
+		requestDao.deleteByReqUUID(requestId2);
+		
+		messageDao.deleteByMsgUUID(messageId);
 	}
 
 	@Test
@@ -136,6 +176,15 @@ public class UserProfileFunctionalTest extends APIFunctionalTest {
 				Assert.assertEquals("mobile1 should get his own private info, expect email to be non-empty","user1@puluo.com", email);
 				Assert.assertEquals("should return user's saying ", "i am user1", saying);
 				Assert.assertEquals("uuid should be " + uuid1, uuid1, uuid);
+				
+				JsonNode private_info = new JsonNode(getStringFromJson(json, "private_info"));
+				List<JsonNode> pendings = getJsonArrayFromJson(private_info, "pending");
+				Assert.assertEquals(requestId2 + " should be the first of the listed pending request", requestId2, getStringFromJson(pendings.get(0), "friend_request","request_id"));
+				Assert.assertEquals(requestId1 + " should be the first of the listed pending request", requestId1, getStringFromJson(pendings.get(1), "friend_request","request_id"));
+				List<JsonNode> messages2 = getJsonArrayFromJson(new JsonNode(getStringFromJson(pendings.get(0), "friend_request")), "messages");
+				Assert.assertTrue("0 message should exist", 0==messages2.size());
+				List<JsonNode> messages1 = getJsonArrayFromJson(new JsonNode(getStringFromJson(pendings.get(1), "friend_request")), "messages");
+				Assert.assertTrue("1 message should exist", 1==messages1.size());
 			}
 
 		});
