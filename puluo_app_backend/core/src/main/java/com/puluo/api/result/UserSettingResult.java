f259dd1324e0c3a0185de8f3bf6e706bed99fd3e
blshao84 <b.l.shao84@gmail.com>
2015-04-30 21:03:17 +0800

    fix typo

--- puluo_app_backend/core/src/main/java/com/puluo/api/result/UserSettingResult.java
+++ puluo_app_backend/core/src/main/java/com/puluo/api/result/UserSettingResult.java
@@ -4,29 +4,29 @@ import com.puluo.util.HasJSON;
 
 public class UserSettingResult extends HasJSON {
 	public String user_uuid;
-	public SettingPai auto_add_friend;
-	public SettingPai allow_stranger_view_timeline;
-	public SettingPai allow_searched;
+	public SettingPair auto_add_friend;
+	public SettingPair allow_stranger_view_timeline;
+	public SettingPair allow_searched;
 	
 	public UserSettingResult(String user_uuid, boolean auto_add_friend,
 			boolean allow_stranger_view_timeline, boolean allow_searched) {
 		super();
 		this.user_uuid = user_uuid;
-		this.auto_add_friend = new SettingPai("允许自动添加好友",auto_add_friend);
-		this.allow_stranger_view_timeline =  new SettingPai("允许陌生人浏览",allow_stranger_view_timeline);
-		this.allow_searched =  new SettingPai("允许被搜索到",allow_searched);
+		this.auto_add_friend = new SettingPair("允许自动添加好友",auto_add_friend);
+		this.allow_stranger_view_timeline =  new SettingPair("允许陌生人浏览",allow_stranger_view_timeline);
+		this.allow_searched =  new SettingPair("允许被搜索到",allow_searched);
 	}
 	
 	public static UserSettingResult dummy() {
 		return new UserSettingResult("", true, true, true);
 	}
 	
-	private class SettingPai {
+	private class SettingPair {
 		@SuppressWarnings("unused")
 		public String name;
 		@SuppressWarnings("unused")
 		public boolean value;
-		public SettingPai(String name, boolean value) {
+		public SettingPair(String name, boolean value) {
 			super();
 			this.name = name;
 			this.value = value;
