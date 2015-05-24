314b2b1e9229eb9620e7a2d4595a83b09ea99ed6
blshao84 <b.l.shao84@gmail.com>
2015-05-24 12:53:22 +0800

    fix dao ....

--- puluo_app_backend/core/src/main/java/com/puluo/dao/impl/PuluoFriendRequestDaoImpl.java
+++ puluo_app_backend/core/src/main/java/com/puluo/dao/impl/PuluoFriendRequestDaoImpl.java
@@ -235,9 +235,9 @@ public class PuluoFriendRequestDaoImpl extends DalTemplate implements
 					.append(" where to_user_uuid = ? and request_status = '")
 				    .append(FriendRequestStatus.Requested.name())
 				    .append("'");
+			sb.append(" order by created_at desc");
 			if(limit > 0) sb.append(" limit ").append(limit);
 			if(offset > 0) sb.append(" offset ").append(offset);
-			sb.append(" order by created_at desc");
 			String selectSQL = sb.toString();
 			log.info(super.sqlRequestLog(selectSQL,userUUID));
 			List<PuluoFriendRequest> entities = reader.query(
