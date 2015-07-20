50c378866433935186519fe7763496a9846e9b52
blshao84 <b.l.shao84@gmail.com>
2015-07-20 09:09:13 +0800

    update curriculum

--- puluo_app_backend/core/src/main/java/com/puluo/config/Configurations.java
+++ puluo_app_backend/core/src/main/java/com/puluo/config/Configurations.java
@@ -26,7 +26,7 @@ public class Configurations {
 	public static final String[] wechatButtonInfo3List = { "yIqSjydNaMkCfo9lbvNMZL_ROCrHi8qEyrHcZ0aL3KQ" };
 	public static final String[] wechatButtonInfo4List = { "yIqSjydNaMkCfo9lbvNMZHAGDNj7WARJFASvOBhpjMk" };
 	public static final String[] wechatButtonInfo5List = { "lnI6lmKccENtVoJ4Ine_dnSvaKURrLMGg6NFYcG9Q2s" };
-	public static final String wechatCurriculum = "5vxnJeFsocaRBpI5ATV3GiVIWK2hp89iQ1MtAa2dUjk";
+	public static final String wechatCurriculum = "UOQ2rbU1z-Bv98o0iNrUnl0LUFQxiHmoe1d_23eUHqM";
 	public static final String emptyImage = Strs.join(imageServer, "empty.jpg");
 
 	public static final Double minPrice = 0.0;
